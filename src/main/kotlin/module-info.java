cd5c9e31b76f40b9d217d82533ef0b4f27dfbd4b
Meodinger Wang <54493746+Meodinger@users.noreply.github.com>
2022-01-18 18:50:22 +0800

    Add module-info.java

---
+++ src/main/kotlin/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * Author: Meodinger
+ * Date: 2022/1/18
+ * Have fun with my code!
+ */
+module ink.meodinger.htmlparser {
+    requires kotlin.stdlib;
+
+    exports ink.meodinger.htmlparser;
+}
